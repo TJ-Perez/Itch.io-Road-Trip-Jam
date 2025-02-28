687dd1a70ade591821cfa02e150f209706b86742
TJ Perez <tjperez090@gmail.com>
2025-02-28 07:49:58 -0800

    basic enemy movement

---
+++ Assets/Scripts/enemyController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class enemyController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject target;
+    [SerializeField] private float health;
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.position = Vector2.MoveTowards(transform.position, target.transform.position, speed);
+    }
+}
