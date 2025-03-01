193aa8713e9a66c11fa4c880e63a99fbfd67d2f0
TJ Perez <tjperez090@gmail.com>
2025-02-28 21:26:57 -0800

    destroy enemy on clicking 10 times

--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -8,8 +8,15 @@ public class enemyController : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] gameController gameController;
 
+    Camera m_Camera;
     private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        m_Camera = Camera.main;
+    }
+
+
     void Start()
     {
         gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
@@ -21,10 +28,23 @@ public class enemyController : MonoBehaviour
     {
         rb.position = Vector2.MoveTowards(transform.position, target.transform.position, speed);
 
-        if(Input.GetMouseButtonDown(0) )
+        if (Input.GetMouseButtonDown(0))
         {
-            health -= gameController.weaponDamage;
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider.gameObject == gameObject)
+            {
+                OnEnemyClicked(gameObject);
+            }
         }
     }
 
+    public void OnEnemyClicked(GameObject gameObject)
+    {
+        health -= gameController.weaponDamage;
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
