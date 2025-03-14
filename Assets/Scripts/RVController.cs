45f4338e7ef4087f5cd5c186969133f075dbac58
TJ Perez <tjperez090@gmail.com>
2025-03-14 11:50:30 -0700

    organize files and create RV tag

---
+++ Assets/Scripts/RVController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class RVController : MonoBehaviour
+{
+
+    [SerializeField] private float totalHealth;
+    [SerializeField] private float currentHealth;
+
+    [SerializeField] FloatingHealthBar floatingHealthBar;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
 
-    [SerializeField] private GameObject target;
+    [SerializeField] private GameObject attackTarget;
     [SerializeField] private float totalHealth;
     [SerializeField] private float currentHealth;
     [SerializeField] private float speed;
@@ -29,9 +29,9 @@ public class EnemyController : MonoBehaviour
         gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
         rb = gameObject.GetComponent<Rigidbody2D>();
 
-        if(target == null)
+        if(attackTarget == null)
         {
-            target = GameObject.FindGameObjectWithTag("Player");
+            attackTarget = GameObject.FindGameObjectWithTag("Player");
         }
 
         if (gameController == null)
@@ -48,9 +48,9 @@ public class EnemyController : MonoBehaviour
             return;
         }
 
-        rb.position = Vector2.MoveTowards(transform.position, target.transform.position, speed);
+        rb.position = Vector2.MoveTowards(transform.position, attackTarget.transform.position, speed);
 
-        if (rb.position.x < target.transform.position.x)
+        if (rb.position.x < attackTarget.transform.position.x)
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
@@ -82,7 +82,7 @@ public class EnemyController : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("RV"))
         {
             Debug.Log("Player Hit");
             enemyAnimator.SetBool("shouldExplode", true);
